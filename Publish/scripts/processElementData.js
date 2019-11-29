ff55762a23f89c7c986f706b5bee3611b9a8aeda
Gerson Júnior <gersonjunior2@outlook.com>
2019-11-28 22:45:41 -0300

    Publicação EPF

---
+++ Publish/scripts/processElementData.js
@@ -0,0 +1,29 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_3AfMkRIlEeqkT69MtqZtfAActivity"]="./../../MobGameDev/capabilitypatterns/resources/MobGameDev_C9E31320_c9e31320_Activity.jpeg"
+contentPage.processPage.elementUrls["_WooCgBInEeqkT69MtqZtfA"] = ["Desenvolver o conceito","MobGameDev/capabilitypatterns/Desenvolver o conceito_AB07926D.html"];
+contentPage.processPage.elementUrls["_VgvNYBInEeqkT69MtqZtfA"] = ["Criar a ideia do jogo","MobGameDev/capabilitypatterns/Criar a ideia do jogo_2B7EDDF8.html"];
+contentPage.processPage.elementUrls["_VgwbgRInEeqkT69MtqZtfA"] = ["Programador","MobGameDev/capabilitypatterns/Programador_EF08952D.html"];
+contentPage.processPage.elementUrls["_Vgv0chInEeqkT69MtqZtfA"] = ["Designer","MobGameDev/capabilitypatterns/Designer_B87132B6.html"];
+contentPage.processPage.elementUrls["_Vgv0cBInEeqkT69MtqZtfA"] = ["Artista","MobGameDev/capabilitypatterns/Artista_BF3A3E90.html"];
+contentPage.processPage.elementUrls["_WoopkBInEeqkT69MtqZtfA"] = ["Engenheiro de Som","MobGameDev/capabilitypatterns/Engenheiro de Som_E44249D6.html"];
+contentPage.processPage.elementUrls["_3AfMkRIlEeqkT69MtqZtfA"] = ["MobGameDev","MobGameDev/capabilitypatterns/MobGameDev_C9E31320.html"];
+contentPage.processPage.elementUrls["_YnxPwBInEeqkT69MtqZtfA"] = ["Arquitetura de Design","MobGameDev/capabilitypatterns/Arquitetura de Design_8AEFB43E.html"];
+contentPage.processPage.elementUrls["_W5vnsBInEeqkT69MtqZtfA"] = ["Desenvolver a prova do conceito","MobGameDev/capabilitypatterns/Desenvolver a prova do conceito_E841D4AB.html"];
+contentPage.processPage.elementUrls["_ZsmJ0BInEeqkT69MtqZtfA"] = ["Suporte ao Jogo","MobGameDev/capabilitypatterns/Suporte ao Jogo_60A032E.html"];
+contentPage.processPage.elementUrls["_YNWkoBInEeqkT69MtqZtfA"] = ["Criar Protótipos","MobGameDev/capabilitypatterns/Criar Protótipos_A85DA042.html"];
+contentPage.processPage.elementUrls["_XuGc0BInEeqkT69MtqZtfA"] = ["Criar um Documento de Design de Jogo (GDD)","MobGameDev/capabilitypatterns/Criar um Documento de Design de Jogo GDD_C5CE32B1.html"];
+contentPage.processPage.elementUrls["_ZBR78BInEeqkT69MtqZtfA"] = ["Desenvolvimento","MobGameDev/capabilitypatterns/Desenvolvimento_F9B391D.html"];
+contentPage.processPage.elementUrls["_ZBTKEBInEeqkT69MtqZtfA"] = ["Testador","MobGameDev/capabilitypatterns/Testador_EB970482.html"];
+contentPage.processPage.elementUrls["_Zee1UBInEeqkT69MtqZtfA"] = ["Testar o Jogo","MobGameDev/capabilitypatterns/Testar o Jogo_6C1005CA.html"];
